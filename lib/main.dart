8774e897a0026732bf8b5b10b3b28f01a4638f62
vamsipesala <31795506+vamsipesala@users.noreply.github.com>
2020-04-29 12:49:01 +0530

    added firebase to android

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final userIdController = TextEditingController();
   final contestIdController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,68 +38,85 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Enter your contest id',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-              ),),
-            TextField(
-              controller: contestIdController,
-              decoration: new InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.greenAccent, width: 5.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 5.0),
-                ),
-                hintText: 'Mobile Number',
-              ),
-            ),
-            SizedBox(height: 20,),
-            Text('Enter your instagram id',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-              ),),
-            TextField(
-              controller: userIdController,
-              decoration: new InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.greenAccent, width: 5.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 5.0),
-                ),
-                hintText: 'Mobile Number',
-              ),
-            ),
-            SizedBox(height: 20,),
-            FlatButton(
-                child: Container(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Enter your contest id',
+                style: TextStyle(
+                  fontSize: 20,
                   color: Colors.black,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text('Submit',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontFamily: 'BalooBhain2',
-                      fontWeight: FontWeight.normal,
-                    ),),
+                ),),
+              TextFormField(
+                controller: contestIdController,
+                decoration: new InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent, width: 5.0),
                   ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 5.0),
+                  ),
+                  hintText: 'Mobile Number',
                 ),
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ScreenshotPage(username: userIdController.text, contestId: contestIdController.text,)),
-                );
-              },
-            )
-          ],
+                  validator: (value) {
+                    print(value);
+                    if (value.isEmpty) {
+                      return 'Please enter username';
+                    }
+                    return null;
+                  }
+              ),
+              SizedBox(height: 20,),
+              Text('Enter your instagram id',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),),
+              TextFormField(
+                controller: userIdController,
+                decoration: new InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent, width: 5.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 5.0),
+                  ),
+                  hintText: 'Mobile Number',
+                ),
+                  validator: (value) {
+                    print(value);
+                    if (value.isEmpty) {
+                      return 'Please enter username';
+                    }
+                    return null;
+                  }
+              ),
+              SizedBox(height: 20,),
+              FlatButton(
+                  child: Container(
+                    color: Colors.black,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text('Submit',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontFamily: 'BalooBhain2',
+                        fontWeight: FontWeight.normal,
+                      ),),
+                    ),
+                  ),
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ScreenshotPage(username: userIdController.text, contestId: contestIdController.text,)),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
